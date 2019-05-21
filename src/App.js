9b038a09a3bfeb9972529b1934bea53d73489e1f
cjislegit <carlosjramirez11@gmail.com>
2019-05-21 13:56:07 -0700

    Added all files for project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowerRouter } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <BrowerRouter>
+      <div className="App">
+        <h1>Project Planner</h1>
+      </div>
+    </BrowerRouter>
+  );
+}
+
+export default App;
