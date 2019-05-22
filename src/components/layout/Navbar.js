129e04a54a8b3499d10b9f52ae351b0f27e8131d
Carlos Ramirez <carlosjramirez11@gmail.com>
2019-05-21 21:13:21 -0700

    Added dashboard, projectlist and notifications

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import SignedinLinks from './SignedinLinks';
+import SignedOutLinks from './SignedOutLinks';
 
 const Navbar = () => {
   return (
-    <nav className="nav-wrapper grey darken-3">
-      <div className="container">
-        <Link to="/" className="brand-logo">
+    <nav className='nav-wrapper grey darken-3'>
+      <div className='container'>
+        <Link to='/' className='brand-logo'>
           Project Planner
         </Link>
         <SignedinLinks />
+        <SignedOutLinks />
       </div>
     </nav>
   );
---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const ProjectList = () => {
+  return (
+    <div className='project-list section'>
+      <div className='card z-depth-0 project-summary'>
+        <div className='card-content grey-text text-darken-3'>
+          <span className='card-title'>Project Title</span>
+          <p>Posted by CJ</p>
+          <p className='grey-text'>21st May</p>
+        </div>
+      </div>
+      <div className='card z-depth-0 project-summary'>
+        <div className='card-content grey-text text-darken-3'>
+          <span className='card-title'>Project Title</span>
+          <p>Posted by CJ</p>
+          <p className='grey-text'>21st May</p>
+        </div>
+      </div>
+      <div className='card z-depth-0 project-summary'>
+        <div className='card-content grey-text text-darken-3'>
+          <span className='card-title'>Project Title</span>
+          <p>Posted by CJ</p>
+          <p className='grey-text'>21st May</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ProjectList;
