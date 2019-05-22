41467c5245ba50b865c7969137348051071e82f9
cjislegit <carlosjramirez11@gmail.com>
2019-05-22 14:06:58 -0700

    Added project details component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
+import ProjectDetails from './components/projects/ProjectDetails';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className='App'>
+      <div className="App">
         <Navbar />
         <Switch>
-          <Route path='/' component={Dashboard} />
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/project/:id" component={ProjectDetails} />
         </Switch>
       </div>
     </BrowserRouter>
