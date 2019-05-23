1a09066198a7f3592dddc00d00991aabd2f87cfc
Carlos Ramirez <carlosjramirez11@gmail.com>
2019-05-23 14:51:54 -0700

    created the create project page

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
 import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+import CreateProejct from './components/projects/CreateProject';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className='App'>
         <Navbar />
         <Switch>
-          <Route exact path="/" component={Dashboard} />
-          <Route path="/project/:id" component={ProjectDetails} />
-          <Route path="/signin" component={SignIn} />
+          <Route exact path='/' component={Dashboard} />
+          <Route path='/project/:id' component={ProjectDetails} />
+          <Route path='/signin' component={SignIn} />
+          <Route path='/signup' component={SignUp} />
+          <Route path='/createproject' component={CreateProejct} />
         </Switch>
       </div>
     </BrowserRouter>
--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
 
 class SignIn extends Component {
-  state = {};
+  state = {
+    email: '',
+    password: ''
+  };
   handleChange = e => {
-    console.log(e);
+    this.setState({
+      [e.target.id]: e.target.value
+    });
   };
   handleSubmit = e => {
-    console.log(e);
+    e.preventDefault();
+    console.log(this.state);
   };
   render() {
     return (
-      <div className="container">
-        <form onSubmit={this.handleSubmit} className="white">
-          <h5 className="grey-text text-darken-3">Sign In</h5>
-          <div className="input-field">
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" onChange={this.handleChange} />
+      <div className='container'>
+        <form onSubmit={this.handleSubmit} className='white'>
+          <h5 className='grey-text text-darken-3'>Sign In</h5>
+          <div className='input-field'>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='email'
+              id='email'
+              onChange={this.handleChange}
+              value={this.state.email}
+            />
           </div>
-          <div className="input-field">
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" onChange={this.handleChange} />
+          <div className='input-field'>
+            <label htmlFor='password'>Password</label>
+            <input
+              type='password'
+              id='password'
+              onChange={this.handleChange}
+              value={this.state.password}
+            />
           </div>
-          <div className="input-field">
-            <button className="btn pink lighten-1 z-depth-0">Sign In</button>
+          <div className='input-field'>
+            <button className='btn pink lighten-1 z-depth-0'>Sign In</button>
           </div>
         </form>
       </div>
