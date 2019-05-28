8d3898091989321b0166c06678ee860fc4412ba9
Carlos Ramirez <carlosjramirez11@gmail.com>
2019-05-27 17:17:56 -0700

    Updated links

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -38,7 +38,7 @@ class SignIn extends Component {
             />
           </div>
           <div className='input-field'>
-            <button className='btn pink lighten-1 z-depth-0'>Sign In</button>
+            <button className='btn z-depth-0'>Sign In</button>
           </div>
         </form>
       </div>
