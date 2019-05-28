5c55a1e9d7ee49371631f660b6af780fb09f421f
cjislegit <carlosjramirez11@gmail.com>
2019-05-28 14:16:22 -0700

    Added thunk and set up actions

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { createProject } from '../../store/actions/projectActions';
 
 class CreateProject extends Component {
   state = {
@@ -12,37 +15,47 @@ class CreateProject extends Component {
   };
   handleSubmit = e => {
     e.preventDefault();
-    console.log(this.state);
+    this.props.createProject(this.state);
   };
   render() {
     return (
-      <div className='container'>
-        <form onSubmit={this.handleSubmit} className='white'>
-          <h5 className='grey-text text-darken-3'>Create New Project</h5>
-          <div className='input-field'>
-            <label htmlFor='title'>Title</label>
+      <div className="container">
+        <form onSubmit={this.handleSubmit} className="white">
+          <h5 className="grey-text text-darken-3">Create New Project</h5>
+          <div className="input-field">
+            <label htmlFor="title">Title</label>
             <input
-              type='text'
-              id='title'
+              type="text"
+              id="title"
               onChange={this.handleChange}
               value={this.state.title}
             />
           </div>
-          <div className='input-field'>
-            <label htmlFor='content'>Project Content</label>
+          <div className="input-field">
+            <label htmlFor="content">Project Content</label>
             <textarea
-              id='content'
-              className='materialize-textarea'
+              id="content"
+              className="materialize-textarea"
               onChange={this.handleChange}
               value={this.state.content}
             />
           </div>
-          <div className='input-field'>
-            <button className='btn pink lighten-1 z-depth-0'>Create</button>
+          <div className="input-field">
+            <button className="btn pink lighten-1 z-depth-0">Create</button>
           </div>
         </form>
       </div>
     );
   }
 }
-export default CreateProject;
+
+const mapDispatchToProps = dispatch => {
+  return {
+    createProject: project => dispatch(createProject(project))
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(CreateProject);
