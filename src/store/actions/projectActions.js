0bf7c6873a39f9bd1467b5c403a904bcc0f39654
cjislegit <carlosjramirez11@gmail.com>
2019-05-29 14:34:00 -0700

    Fixed firestore error

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,23 @@
+import CreateProject from '../../components/projects/CreateProject';
+
 export const createProject = project => {
-  return (dispatch, getState, { getFirebase, getFireStore }) => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     //make async call to database
-    dispatch({ type: 'CREATE_PROJECT', project: project });
+    const firestore = getFirestore();
+    firestore
+      .collection('projects')
+      .add({
+        ...project,
+        authorFirstName: 'CJ',
+        authorLastName: 'Ramirez',
+        authorId: 12345,
+        createdAt: new Date()
+      })
+      .then(() => {
+        dispatch({ type: 'CREATE_PROJECT', project: project });
+      })
+      .catch(err => {
+        dispatch({ type: 'CREATE_PROJECT_ERROR', err });
+      });
   };
 };
