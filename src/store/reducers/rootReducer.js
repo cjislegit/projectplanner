87d599a5e009b9346eddec6f68a3deaabdc0001d
cjislegit <carlosjramirez11@gmail.com>
2019-05-30 14:04:27 -0700

    Update version

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,11 +1,14 @@
 import { combineReducers } from 'redux';
+import { firestoreReducer } from 'redux-firestore';
 
 import authReducer from './authReducer';
 import projectReducer from './projectReducer';
+import { firebaseReducer } from 'react-redux-firebase';
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  project: projectReducer
+  project: projectReducer,
+  firestore: firestoreReducer
 });
 
 export default rootReducer;
