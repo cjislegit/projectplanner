7d7e999ed81a59f9c5d703b16167e472e0827026
cjislegit <carlosjramirez11@gmail.com>
2019-06-04 16:57:30 -0700

    Added ability to create new account

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -12,6 +12,18 @@ const authReducer = (state = initState, action) => {
     case 'SIGNOUT_SUCCESS':
       console.log('signout success');
       return state;
+    case 'SIGNUP_SUCCESS':
+      console.log('signup success');
+      return {
+        ...state,
+        authError: null
+      };
+    case 'SIGNUP_Error':
+      console.log('signup error');
+      return {
+        ...state,
+        authError: action.err.message
+      };
 
     default:
       return state;
