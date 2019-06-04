000344aecce28de3610a173df0f2a9dcd4f16c65
cjislegit <carlosjramirez11@gmail.com>
2019-06-04 14:14:05 -0700

    When logged in user cannot get to the sign in or sign up pages

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 class SignUp extends Component {
   state = {
@@ -17,52 +19,59 @@ class SignUp extends Component {
     console.log(this.state);
   };
   render() {
+    const { auth } = this.props;
+    if (auth.uid) return <Redirect to="/" />;
     return (
-      <div className='container'>
-        <form onSubmit={this.handleSubmit} className='white'>
-          <h5 className='grey-text text-darken-3'>Sign In</h5>
-          <div className='input-field'>
-            <label htmlFor='email'>Email</label>
+      <div className="container">
+        <form onSubmit={this.handleSubmit} className="white">
+          <h5 className="grey-text text-darken-3">Sign In</h5>
+          <div className="input-field">
+            <label htmlFor="email">Email</label>
             <input
-              type='email'
-              id='email'
+              type="email"
+              id="email"
               onChange={this.handleChange}
               value={this.state.email}
             />
           </div>
-          <div className='input-field'>
-            <label htmlFor='password'>Password</label>
+          <div className="input-field">
+            <label htmlFor="password">Password</label>
             <input
-              type='password'
-              id='password'
+              type="password"
+              id="password"
               onChange={this.handleChange}
               value={this.state.password}
             />
           </div>
-          <div className='input-field'>
-            <label htmlFor='firstName'>First Name</label>
+          <div className="input-field">
+            <label htmlFor="firstName">First Name</label>
             <input
-              type='text'
-              id='firstName'
+              type="text"
+              id="firstName"
               onChange={this.handleChange}
               value={this.state.firstName}
             />
           </div>
-          <div className='input-field'>
-            <label htmlFor='lastName'>Last Name</label>
+          <div className="input-field">
+            <label htmlFor="lastName">Last Name</label>
             <input
-              type='text'
-              id='lastName'
+              type="text"
+              id="lastName"
               onChange={this.handleChange}
               value={this.state.lastName}
             />
           </div>
-          <div className='input-field'>
-            <button className='btn z-depth-0'>Sign Up</button>
+          <div className="input-field">
+            <button className="btn z-depth-0">Sign Up</button>
           </div>
         </form>
       </div>
     );
   }
 }
-export default SignUp;
+const mapStateToProps = state => {
+  return {
+    auth: state.firebase.auth
+  };
+};
+export default connect(mapStateToProps)(SignUp);
