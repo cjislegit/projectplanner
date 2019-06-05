e40af54aef444349606f83ceb6f6ed50e98aeda5
cjislegit <carlosjramirez11@gmail.com>
2019-06-04 17:59:00 -0700

    project summary is now dynamic

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -5,7 +5,9 @@ const ProjectSummary = ({ project }) => {
     <div className="card z-depth-0 project-summary">
       <div className="card-content grey-text text-darken-3">
         <span className="card-title">{project.title}</span>
-        <p>Posted by CJ</p>
+        <p>
+          {project.authorFirstName} {project.authorLastName}
+        </p>
         <p className="grey-text">22st May</p>
       </div>
     </div>
